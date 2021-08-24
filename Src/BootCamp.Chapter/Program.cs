bce787b16b34c0581dc9356292217d49d6a18ba9
TedHenschen <tedhenschen@gmail.com>
2021-08-24 06:56:55 -0400

    notes - added lesson notes lesson notes added to the top of the program file, detailing about abstract and virtual methods.

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -2,6 +2,28 @@
 
 namespace BootCamp.Chapter
 {
+
+    /*
+     * Virtual Methods (polymorphism) - 
+     *      Virtual is a Keyword meaning that method is not final and implementation can be changed in a child, 
+     *      means that it can be overriden!
+     *      Virtual Method can be called from the child class using base.method()
+     *      
+     *      
+     *  Abstract 
+     *      Can be used for either classes or methods (virtual can only be used in methods.)
+     *      class - cannot be instantiated on its own, needs a child class to instantiate
+     *      
+     *      method - declared in parent, and implemented in child.  A type of virtual method
+     *      
+     *      The procted access modifier is used for abstract class mathods, because it will never be used pubilicly
+     *      and will always only be called by the child class.
+     *      
+     *  Virtual methods should be avoided when possible, except when it feels very natural and with good justification
+     *  
+     *  Abstract is a better option than virtual, but the use of it shouldn't be forced.
+     *  
+     */
     class Program
     {
         static void Main(string[] args)
@@ -73,7 +95,6 @@ namespace BootCamp.Chapter
             Console.WriteLine("Myteriously teaching.");
         }
     }
-
     public class GoodTeacher : Teacher
     {
         public override void Work()
@@ -90,8 +111,6 @@ namespace BootCamp.Chapter
             Console.WriteLine("Loving my job.");
         }
     }
-
-   
     public class BadTeacher : Teacher
     {
         public override void Work()
@@ -109,6 +128,7 @@ namespace BootCamp.Chapter
             return _name;
         }
 
+        //procted is used for abstract class mathods, because it will never be used pubilicly and will always only be called by the child class.
         protected Personality(string name)
         {
             Console.WriteLine("1) Personality");
@@ -129,7 +149,6 @@ namespace BootCamp.Chapter
             Console.WriteLine("Today sucks... I don't want to go to work...");
         }
     }
-
     class Optimist : Personality
     {
         public Optimist() : base("Optimist")
@@ -142,7 +161,6 @@ namespace BootCamp.Chapter
             Console.WriteLine("What a beautiful day! Be my friend! :)");
         }
     }
-
     class SadOptimist : Optimist
     {
         public SadOptimist()
