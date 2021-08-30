b987b2b9e11dde073491335d8301785b0cef77d4
TedHenschen <tedhenschen@gmail.com>
2021-08-30 05:53:18 -0400

    commit - Notes added

---
+++ Src/BootCamp.Chapter/Notes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Notes
+    {
+        /*
+         * Chapter 3
+         * Lesson 2: Struct, in, out, ref
+         * 
+         *  Struct - 
+         *      
+         *      Use a struct over a class If it isn't a service, it is 3 or less fields, and once initialized it won't change
+         *      
+         *      Value Objects - Something that refers to a single or multiple simple primitive values
+         *      some good examples where structs could be used are for phone numbers, a tightly related group of data
+         *      that makes up an object, that is small and simple.
+         *      
+         *      values must be assigned when it is initialized.
+         *      
+         *      Can have a method, but they should be simple and self reliant
+         *      
+         *      struct PhoneNumber
+         *      {
+         *          public readonly string Number;
+         *          public readonly string AreaCode;
+         *          public readonly string Country;
+         *          
+         *          public PhoneNumber (string number, string areaCode, string country)
+         *          {
+         *              Number =  number;
+         *              AreaCode = areaCode;
+         *              Country = country;
+         *          }
+         *      {
+         *      
+         *      
+         *  ref, in, out
+         *  
+         *  Ref - allowes even value types to be passed by reference.
+         *      
+         *      
+         * 
+         */
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
